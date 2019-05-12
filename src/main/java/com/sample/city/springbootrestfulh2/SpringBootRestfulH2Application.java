71ba2995d18da10cb580e18723efb30f9f2ba924
Derek Chen <derekgc@icloud.com>
2019-05-12 11:40:47 -0400

    commit

---
+++ src/main/java/com/sample/city/springbootrestfulh2/SpringBootRestfulH2Application.java
@@ -0,0 +1,13 @@
+package com.sample.city.springbootrestfulh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulH2Application.class, args);
+	}
+
+}
